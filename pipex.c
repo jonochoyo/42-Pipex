a2e80c069d3124771ef1e4e00a20e82f87eee4e1
Jonathan Choy <jonochoyo@gmail.com>
2023-09-15 17:00:34 +0930

    Edit pipex to work wit args

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: jchoy-me <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 16:58:45 by jchoy-me          #+#    #+#             */
-/*   Updated: 2023/09/15 15:29:14 by jchoy-me         ###   ########.fr       */
+/*   Updated: 2023/09/15 16:59:55 by jchoy-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,68 @@
 // Once it finds PATH=, gets all the available paths in an array of strings
 // It checks if the first command can be found in one of the paths
 // If can be found, it runs the program with all the flags (separated by space)
-// To get all the flags will need an array that starts with the path, and then has all the flags as separate elements (:)
+// To get all the flags will need an array that starts with the path, and then 
+//has all the flags as separate elements (:)
 // The array of the path and flags is NULL terminated too.  
 
-/*
-Test for pipex working without envp.
-Programs are set and won't read the user input arguments.
-Program can read user input for infile and outfile
--Create pipe with two fd
--Fork into parent and child
-*/
+// Function to get the path of the command in the env variables PATH
+// Iterates through the envp's until find PATH
+// Split PATH variable into all_paths with split showing all available paths
+// Join / with the cmd to then join with a path to check if the cmd exists
+// and can be run with access. If it does, we return the full path. 
+// Otherwise we free the created things and return NULL if command not found. 
+char	*get_path(char *cmd, char *envp[])
+{
+	int		i;
+	char	**all_paths;
+	char	*tmp;
+	char	*path;
+
+	i = 0;
+	while (envp[i] != 0 && (ft_strncmp(envp[i], "PATH=", 5) != 0))
+		i++;
+	all_paths = ft_split(envp[i] + 5, ':');
+	tmp = ft_strjoin("/", cmd);
+	i = 0;
+	while (all_paths[i])
+	{
+		path = ft_strjoin(all_paths[i], tmp);
+		if (access(path, F_OK | X_OK) == 0)
+		{
+			free(tmp);
+			return (path);
+		}
+		free(path);
+		i++;
+	}
+	return (NULL);
+}
+
+void	execute_cmd(char *cmd, char *envp[])
+{
+	char	*path;
+	char	**cmd_flags;
+
+	cmd_flags = ft_split(cmd, ' ');
+	path = get_path(cmd_flags[0], envp);
+	if (path == NULL)
+	{
+		perror("Command not found:");
+		exit(1);
+	}
+	if (execve(path, cmd_flags, envp) == -1)
+	{
+		perror("Could not execute the command");
+		free(cmd_flags);
+		free(path);
+		exit(1);
+	}
+	free(cmd_flags);
+	free(path);
+}
+
+// Function to execute the command
+// Split the input cmd into an array of strings with the cmd and flags
 
 // 	CHILD (Working, need to add envps)
 // -If its child, we make the first command run there by taking
@@ -34,18 +86,16 @@ Program can read user input for infile and outfile
 // of the pipe
 // -We ensure fds from pipe and child are closed
 // -Execute cmd1 in the child using infile.
-
-void	child_prcs(char *infile, int *pipe_fd, char *cmd11[])
+void	child_prcs(char *infile, int *pipe_fd, char *cmd, char *envp[])
 {
 	int	in_fd;
-	int val_child;
 
 	in_fd = open(infile, O_RDONLY);
 	if (in_fd == -1)
 	{
 		perror("Open infile error:");
 		exit(1);
-	}	
+	}
 	// Make stdin to be the infile
 	dup2(in_fd, STDIN_FILENO);
 	close(in_fd);
@@ -53,9 +103,7 @@ void	child_prcs(char *infile, int *pipe_fd, char *cmd11[])
 	dup2(pipe_fd[1], STDOUT_FILENO);
 	close(pipe_fd[0]);
 	// Execute command 1
-	val_child = execve(cmd11[0], cmd11, NULL);
-	if (val_child == -1)
-		perror("Executing child error:");
+	execute_cmd(cmd, envp);
 }
 
 // PARENT
@@ -67,10 +115,9 @@ void	child_prcs(char *infile, int *pipe_fd, char *cmd11[])
 // close parent_fd
 // execute cmd22 for parent so it outputs to the outfile by using
 // the pipe read end as input.
-void	parent_prcs(char *outfile, int *pipe_fd, char *cmd22[])
+void	parent_prcs(char *outfile, int *pipe_fd, char *cmd, char *envp[])
 {
 	int	out_fd;
-	int	val_parent;
 
 	wait(NULL);
 	out_fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
@@ -78,83 +125,44 @@ void	parent_prcs(char *outfile, int *pipe_fd, char *cmd22[])
 	{
 		perror("Open outfile error:");
 		exit(1);
-	}		
+	}
 	// Make input to be the pipe read end
 	dup2(pipe_fd[0], STDIN_FILENO);
 	close(pipe_fd[1]);
 	// Make output to be the outfile
 	dup2(out_fd, STDOUT_FILENO);
-	close(out_fd); 
-	val_parent = execve(cmd22[0], cmd22, NULL);
-	if (val_parent == -1)
-		perror("Executing parent error:");
+	close(out_fd);
+	execute_cmd(cmd, envp);
 }
 
+//argv[1]: infile
+//argv[2]: cmd1
+//argv[3]: cmd2
+//argv[4]: outfile
 int	main(int argc, char *argv[], char *envp[])
 {
-	char	*cmd1;
-	char	*cmd2;
 	pid_t	pid;
+	int		pipe_fd[2];
 
-	(void)envp;
 	if (argc != 5)
 	{
-		perror("Invalid input:");
+		perror("Invalid input: Try: ./pipex file1 cmd1 cmd2 file2");
 		exit(1);
 	}
-	cmd1 = argv[2];
-	cmd2 = argv[3];
-	// We have to find the correct path for the command input by the user
-	// Testing if user inputs ls, we will assign the path to ls
-	// If no input, default to cat
-	// May need to create an array of strings where the first string
-	// is the path and the rest the flags so that can be the input for execve
-	char *path;
-	if (ft_strncmp(cmd1, "ls", 2) == 0)
-		path = "/bin/ls";
-	else
-		path = "/bin/cat";
-	// if cannot access the program or is not an executable we exit.
-	if (access(path, F_OK | X_OK) != 0)
-		exit(1);
-
-	char	*cmd11[] = {path, NULL};
-	char	*cmd22[] = {"/usr/bin/wc", "-l", NULL};
-
-	int	pipe_fd[2];
 	if (pipe(pipe_fd) == -1)
+	{
+		perror("Could not create pipe");
 		exit(1);
-
+	}
 	pid = fork();
 	if (pid == -1)
+	{
+		perror("Error while forking:");
 		exit(1);
-
+	}
 	if (pid == 0)
-	{
-		//argv[1] is infile
-		child_prcs(argv[1], pipe_fd, cmd11);
-	}
+		child_prcs(argv[1], pipe_fd, argv[2], envp);
 	if (pid > 0)
-	{
-		//argv[4] is outfile
-		parent_prcs(argv[4], pipe_fd, cmd22);
-	}
+		parent_prcs(argv[4], pipe_fd, argv[3], envp);
 	return (0);
 }
-
-//envp testing
-// int	main(int argc, char *argv[], char *envp[])
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	int file = open("dup.txt", O_WRONLY | O_CREAT, 0777);
-// 	dup2(file, STDOUT_FILENO);
-// 	close(file);
-// 	printf("%s\n", envp[4]);
-// 	while(envp[i] != NULL)
-// 	{
-// 		printf("%s\n", envp[i]);
-// 		i++;
-// 	}
-// }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: jchoy-me <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 15:48:31 by jchoy-me          #+#    #+#             */
-/*   Updated: 2023/09/12 16:43:26 by jchoy-me         ###   ########.fr       */
+/*   Updated: 2023/09/15 16:55:45 by jchoy-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 // Temporary use of functions which will be replaced with libft"
 size_t	ft_strlen(const char *s);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 
